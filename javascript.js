c516dee852ad1c8fdef93c0f955e2ef2d824ddfa
pantazis vastardis <pantazisvastardis@yahoo.com>
2016-08-15 14:49:08 +0300

    x

--- javascript.js
+++ javascript.js
@@ -1,22 +1,23 @@
  SC.initialize({
-    client_id: '7fcf1c871e016ce004ecc1dcbb520d08',
-    redirect_uri: 'https://pantazis.github.io/My-Angularjs-App/callback.html'
-  });
+     client_id: '7fcf1c871e016ce004ecc1dcbb520d08',
+     redirect_uri: 'https://pantazis.github.io/My-Angularjs-App/callback.html'
+ });
+
+ $("#loadTracks").on("click", function () {
+     console.log("hello");
+     
     
-    $("#loadTracks").on("click", function(){
-        console.log("hello");
-    var adde = SC.get("/tracks", {limit: 1});
-         console.log(adde);
-       
-    SC.get("/tracks", {limit: 10}, function(tracks){
-       
-      var track = tracks[2];
-        console.log(track);
-      SC.oEmbed(track.uri, document.getElementById("track"));
-    });*/
-  });
 
-window.onload = function(){SC.initialize();SC.get();};
+     SC.get("/tracks", {
+         limit: 10
+     }, function (tracks) {
 
+         var track = tracks[2];
+         console.log(track);
+         SC.oEmbed(track.uri, document.getElementById("track"));
+     });
+ });
 
-
+ window.onload = function () {
+     SC.initialize();
+ };
