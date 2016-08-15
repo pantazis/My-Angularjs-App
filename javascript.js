b268134bfa47377cd03870ed57431009e5f39d3a
pantazis vastardis <pantazisvastardis@yahoo.com>
2016-08-15 13:27:32 +0300

    xxxxxx

--- javascript.js
+++ javascript.js
@@ -3,8 +3,8 @@
     redirect_uri: 'https://pantazis.github.io/My-Angularjs-App/callback.html'
   });
     
-    SC.get('/msmrsounds/tracks').then(function(tracks){
+    SC.get('/user/whose-misery-is-it').then(function(tracks){
         
-  console.log('Latest track: ' + tracks[0].title);
+  console.log( tracks[]);
 });
-    console.log(SC);
+    console.dir(SC);
