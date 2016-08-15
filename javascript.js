81dcddf3653253edbc1e58c9dfa34a2c27279096
pantazis vastardis <pantazisvastardis@yahoo.com>
2016-08-15 12:33:59 +0300

    xxx

--- javascript.js
+++ javascript.js
@@ -3,7 +3,8 @@
     redirect_uri: 'https://pantazis.github.io/My-Angularjs-App/callback.html'
   });
     
-    SC.get('/user/msmrsounds/tracks').then(function(tracks){
+    SC.get('/msmrsounds/tracks').then(function(tracks){
+        
   console.log('Latest track: ' + tracks[0].title);
 });
     console.log(SC);
