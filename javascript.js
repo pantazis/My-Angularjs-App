566affb4105cef252c98d58a0e1b127ec2c75f94
pantazis vastardis <pantazisvastardis@yahoo.com>
2016-08-15 14:05:58 +0300

    xxxxxxxx

--- javascript.js
+++ javascript.js
@@ -4,10 +4,15 @@
   });
     
     $("#loadTracks").on("click", function(){
-        console.log("hello");
-  
+        
+    SC.get("/tracks", {limit: 1}, function(tracks){
+       
+      var track = tracks[0];
+      SC.oEmbed(track.uri, document.getElementById("track"));
     });
-  
+  });
+
+window.onload = function(){SC.initialize();};
 
 
 
