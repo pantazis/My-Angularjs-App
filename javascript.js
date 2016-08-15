7dd587b728ec7b513b9d32fdca487e0d6a59e4b3
pantazis vastardis <pantazisvastardis@yahoo.com>
2016-08-15 19:28:36 +0300

    xxxxx

--- javascript.js
+++ javascript.js
@@ -4,9 +4,13 @@ window.onload = function () {
         client_id: '7fcf1c871e016ce004ecc1dcbb520d08',
         redirect_uri: 'https://pantazis.github.io/My-Angularjs-App/callback'
     });
-
-  SC.get('/user/183/tracks').then(function(tracks){
-  alert('Latest track: ' + tracks[0].title);
+SC.get('/resolve', {
+  url: 'https://soundcloud.com/paulosman'
+}, function(user) {
+  console.log(user.id);
 });
+ /* SC.get('/user/183/tracks').then(function(tracks){
+  alert('Latest track: ' + tracks[0].title);
+});*/
 };
 
