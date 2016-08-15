ca897916d9252b2dc9388466e686d111683a04e8
pantazis vastardis <pantazisvastardis@yahoo.com>
2016-08-15 12:28:03 +0300

    xxxxxx

---
+++ javascript.js
@@ -0,0 +1,9 @@
+ SC.initialize({
+    client_id: '7fcf1c871e016ce004ecc1dcbb520d08',
+    redirect_uri: 'https://pantazis.github.io/My-Angularjs-App/callback.html'
+  });
+    
+    SC.get('/user/msmrsounds/tracks').then(function(tracks){
+  console.log('Latest track: ' + tracks[0].title);
+});
+    console.log(SC);
