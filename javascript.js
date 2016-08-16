e50029b045ec86779795b12a6007cc9337779bd8
pantazis vastardis <pantazisvastardis@yahoo.com>
2016-08-16 17:35:08 +0300

    x

--- javascript.js
+++ javascript.js
@@ -4,24 +4,25 @@
         client_id: '7fcf1c871e016ce004ecc1dcbb520d08',
         redirect_uri: 'https://pantazis.github.io/My-Angularjs-App/callback'
     });
-  /*var asutis = SC.get('/resolve', {
-  url: 'https://soundcloud.com/hbrideacast'
-});
-    console.log(asutis);
-  
-    http://api.soundcloud.com/tracks/13158665?client_id=7fcf1c871e016ce004ecc1dcbb520d08
-    */
 
- console.log(SC.get('/tracks/13158655'));
 
-function getMusic(genre){
-    SC.get("tracks",{
-        genres:genre
-    },function(tracks){
-        console.log(tracks);
-    });
-};
-getMusic('punk');
+
+
+  SC.get("tracks", { genre:"Hip-hop & Rap"
+      
+      
+  }).then(function (tracks) {
+      SC.oEmbed(tracks[0].uri, {
+          auto_play: true
+      }).then(function(embed){
+ document.getElementById("track").innerHTML= embed.html;
+          
+}
+      );
+  });
+
+
+
 
 
 
