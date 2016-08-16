94667be2f32cf4f964678f6a56904da2b9f0a29d
pantazis vastardis <pantazisvastardis@yahoo.com>
2016-08-16 10:11:59 +0300

    x

--- javascript.js
+++ javascript.js
@@ -12,7 +12,7 @@ window.onload = function () {
     http://api.soundcloud.com/tracks/13158665?client_id=7fcf1c871e016ce004ecc1dcbb520d08
     */
 
- SC.get('/user/37590655/tracks').then(function(tracks){
+ SC.get('/tracks/13158655').then(function(tracks){
   alert('Latest track: ' + tracks[0].title);
 });
 };
