8920bc86fc794c7c90bee2b250e0759e700beb17
pantazis vastardis <pantazisvastardis@yahoo.com>
2016-08-16 10:07:07 +0300

    xxxx

--- javascript.js
+++ javascript.js
@@ -4,13 +4,16 @@ window.onload = function () {
         client_id: '7fcf1c871e016ce004ecc1dcbb520d08',
         redirect_uri: 'https://pantazis.github.io/My-Angularjs-App/callback'
     });
-var asutis = SC.get('/resolve', {
+  /*var asutis = SC.get('/resolve', {
   url: 'https://soundcloud.com/hbrideacast'
 });
     console.log(asutis);
+  
+    http://api.soundcloud.com/tracks/13158665?client_id=7fcf1c871e016ce004ecc1dcbb520d08
+    */
 
- /*SC.get('/user/37590655/tracks').then(function(tracks){
+ SC.get('/user/37590655/tracks').then(function(tracks){
   alert('Latest track: ' + tracks[0].title);
-});*/
+});
 };
 
