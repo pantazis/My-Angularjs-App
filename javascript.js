346e3c57e0a73908f8326c6ee4e34b518a5845c8
pantazis vastardis <pantazisvastardis@yahoo.com>
2016-08-16 10:22:32 +0300

    asasaa

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
-window.onload = function () {
-    console.log( SC);
+
+
     SC.initialize({
         client_id: '7fcf1c871e016ce004ecc1dcbb520d08',
         redirect_uri: 'https://pantazis.github.io/My-Angularjs-App/callback'
@@ -12,8 +12,6 @@ window.onload = function () {
     http://api.soundcloud.com/tracks/13158665?client_id=7fcf1c871e016ce004ecc1dcbb520d08
     */
 
- SC.get('/tracks/13158655').then(function(tracks){
-  alert('Latest track: ' + tracks[0].title);
-});
-};
+ console.log(SC.get('/tracks/13158655'));
+
 
