6f34e672a373473bda6d24ee10edb1cdb92b94f1
pantazis vastardis <pantazisvastardis@yahoo.com>
2016-08-17 11:55:42 +0300

    x

--- javascript.js
+++ javascript.js
@@ -1,17 +1,21 @@
+SC.initialize({
+    client_id: '7fcf1c871e016ce004ecc1dcbb520d08',
+    redirect_uri: 'https://pantazis.github.io/My-Angularjs-App/callback'
+});
 
+//https://api-v2.soundcloud.com/charts?kind=top&genre=soundcloud:genres:all-music&client_id=02gUJC0hH2ct1EGOcYXQIzRFU91c72Ea&limit=20&offset=0&linked_partitioning=1&app_version=1471261044
+//https://api-v2.soundcloud.com/tracks?kind=top&genre=soundcloud:genres:classical&client_id=02gUJC0hH2ct1EGOcYXQIzRFU91c72Ea&limit=20&offset=0&linked_partitioning=1&app_version=1471261044
+https://api.soundcloud.com/tracks?genre=soundcloud:genres:all-music&format=json&client_id=7fcf1c871e016ce004ecc1dcbb520d08
 
-    SC.initialize({
-        client_id: '7fcf1c871e016ce004ecc1dcbb520d08',
-        redirect_uri: 'https://pantazis.github.io/My-Angularjs-App/callback'
-    });
-
-
-
-
-  SC.get("tracks", { genre:"Hip-hop & Rap"
-      
-      
-  }).then(function (tracks) {
+  SC.get("charts",{
+    kind: "top",
+    genre: "soundcloud:genres:classical",
+    client_id: '7fcf1c871e016ce004ecc1dcbb520d08',
+    limit: 20,
+    offset: 0,
+    linked_partitioning: 1,
+    app_version: 1471261044
+}).then(function (tracks) {
       SC.oEmbed(tracks[0].uri, {
           auto_play: true
       }).then(function(embed){
@@ -20,11 +24,5 @@
 }
       );
   });
-
-
-
-
-
-
 
 
