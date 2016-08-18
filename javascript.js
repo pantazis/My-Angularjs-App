fb76802c79930b39be5418af66384f8e52c7e89c
pantazis vastardis <pantazisvastardis@yahoo.com>
2016-08-18 16:41:08 +0300

    x

--- javascript.js
+++ javascript.js
@@ -21,4 +21,4 @@ SC.initialize({
   });
 
 
-https://api-v2.soundcloud.com/charts?kind=top&genre=soundcloud:genres:ambient&client_id=02gUJC0hH2ct1EGOcYXQIzRFU91c72Ea&limit=20&offset=0&linked_partitioning=1&app_version=1471511869
+//https://api-v2.soundcloud.com/charts?kind=top&genre=soundcloud:genres:ambient&client_id=02gUJC0hH2ct1EGOcYXQIzRFU91c72Ea&limit=20&offset=0&linked_partitioning=1&app_version=1471511869
