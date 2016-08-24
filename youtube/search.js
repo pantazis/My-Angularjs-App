4e4557849b79579862f5572f2d444cc15d80b77c
pantazis vastardis <pantazisvastardis@yahoo.com>
2016-08-24 12:19:32 +0300

    xxx

--- youtube/search.js
+++ youtube/search.js
@@ -5,7 +5,7 @@
 function showResponse(response) {
     var responseString = JSON.stringify(response, '', 2);
     document.getElementById('response').innerHTML += responseString;
-
+    
 }
 
 // Called automatically when JavaScript client library is loaded.
@@ -21,13 +21,20 @@ function onYouTubeApiLoad() {
 
     search();
 }
-
+function searchByKeyword() {
+  var results = YouTube.Search.list('id,snippet', {q: 'dogs', maxResults: 25});
+  for(var i in results.items) {
+    var item = results.items[i];
+    Logger.log('[%s] Title: %s', item.id.videoId, item.snippet.title);
+  }
+}
 
 function search() {
     // Use the JavaScript client library to create a search.list() API call.
-    var request = gapi.client.youtube.Search.list('id,snippet', {q: 'dogs', maxResults: 25});
-
-
+    var request = gapi.client.youtube.search.list({
+        part: 'id',
+    });
+    
     // Send the request to the API server,
     // and invoke onSearchRepsonse() with the response.
     request.execute(onSearchResponse);
@@ -35,6 +42,6 @@ function search() {
 
 // Called automatically with the response of the YouTube API request.
 function onSearchResponse(response) {
-    showResponse(response);
-
+   showResponse(response);
+    
 }
