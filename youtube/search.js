22dd75cb3ca9a3ba13458ba7ac60327ea4f4691f
pantazis vastardis <pantazisvastardis@yahoo.com>
2016-08-24 13:50:48 +0300

    ap key

--- youtube/search.js
+++ youtube/search.js
@@ -1,3 +1,25 @@
 /*
 AIzaSyDadPNzC_v8PZ9Fa8WRNNSa2Wx34LDu5l0
 */
+function googleApiClientReady() {
+
+    var apiKey = 'AIzaSyDadPNzC_v8PZ9Fa8WRNNSa2Wx34LDu5l0';
+
+    gapi.client.setApiKey(apiKey);
+    gapi.client.load('youtube', 'v3', function() {
+
+        request = gapi.client.youtube.search.list({
+            part: 'snippet',
+            channelId: 'UCqhNRDQE_fqBDBwsvmT8cTg',
+            order: 'date',
+            type: 'video'
+
+        });
+
+        request.execute(function(response) {
+                console.log(response);
+
+        });
+    });
+
+}
