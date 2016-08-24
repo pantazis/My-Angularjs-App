0d27139e21829fc011c3847a9cb51b47e6851ef8
pantazis vastardis <pantazisvastardis@yahoo.com>
2016-08-24 14:43:15 +0300

    xxx

--- youtube/search.js
+++ youtube/search.js
@@ -4,5 +4,9 @@ AIzaSyDadPNzC_v8PZ9Fa8WRNNSa2Wx34LDu5l0
 gapi.load('client', init);
 
 function init() {
-    console.log("hello")
+   gapi.client.setApiKey('AIzaSyDadPNzC_v8PZ9Fa8WRNNSa2Wx34LDu5l0');
+    gapi.client.load('youtube', 'v3',soreos());
 };
+function soreos(){
+        console.log("hello");
+    };
