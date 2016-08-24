d9559d2f3ccb1f1f0d7e487aba543e742bd6c0ae
pantazis vastardis <pantazisvastardis@yahoo.com>
2016-08-24 12:15:25 +0300

    xx

--- youtube/search.js
+++ youtube/search.js
@@ -25,13 +25,8 @@ function onYouTubeApiLoad() {
 
 function search() {
     // Use the JavaScript client library to create a search.list() API call.
-    var request = gapi.client.youtube.search.list({
-        part: 'id',
-        {
-            q: 'dogs',
-            maxResults: 25
-        }
-    });
+    var request = gapi.client.youtube.Search.list('id,snippet', {q: 'dogs', maxResults: 25});
+
 
     // Send the request to the API server,
     // and invoke onSearchRepsonse() with the response.
